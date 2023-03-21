3b0973c15810a258808441215c9204dfbb1b95e3
R4NP3R <ssrafa.2014@gmail.com>
2023-03-20 23:13:46 -0300

    exercicio de TypeScript

---
+++ typescript.js
@@ -0,0 +1,10 @@
+"use strict";
+function multiplicador(NumeroA, NumeroB) {
+    return NumeroA * NumeroB;
+}
+function olaPessoa(NomedaPessoa) {
+    console.log(`Olá ${NomedaPessoa}`);
+}
+const dezxDez = multiplicador(10, 10);
+console.log(dezxDez);
+const olaManoel = olaPessoa('manoel');
---
+++ typescript.ts
@@ -0,0 +1,12 @@
+function multiplicador(NumeroA: number, NumeroB: number) {
+    return NumeroA * NumeroB;
+}
+
+function olaPessoa(NomedaPessoa: string) {
+    console.log(`Olá ${NomedaPessoa}`);
+}
+
+const dezxDez = multiplicador(10,10);
+
+console.log(dezxDez);
+const olaManoel = olaPessoa('manoel');
